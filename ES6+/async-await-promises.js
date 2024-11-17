f6492808ea09de30007a9127b83e548c5a415dd3
Amit Gangwar <gangwaramit449@gmail.com>
2024-11-17 13:14:58 +0530

    Added Async Await

--- ES6+/async-await-promises.js
+++ ES6+/async-await-promises.js
@@ -24,17 +24,53 @@ getData();
  Rejected: The operation failed.
 */
 
-myPromise
-  .then((res) => {
-    console.log(res); // operation successful
-  })
-  .catch((error) => {
-    console.log(error); // operation failed
-  })
-  .finally(() => {
-    console.log("Operation Successful !!"); // doesnot depends on success or failure
-  });
+// myPromise
+//   .then((res) => {
+//     console.log(res); // operation successful
+//   })
+//   .catch((error) => {
+//     console.log(error); // operation failed
+//   })
+//   .finally(() => {
+//     console.log("Operation Successful !!"); // doesnot depends on success or failure
+//   });
 
 // Example
+const fetchData = () => {
+  fetch("https://dummyjson.com/users")
+    .then((response) => {
+      if (!response.ok) {
+        console.log("No Response !!");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("Error !!");
+    });
+};
 
-//*
+fetchData();
+
+//* Async-await
+/*
+ Async -> function that returns a promise . Inside an async function , you can use the await keyword to pause the sececution until a promise is resolved or reject.
+ await -> used an async function. It makes js wait until the promise resolveds and returns its result
+*/
+
+const fetchUser = async () => {
+  try {
+    const res = await fetch("https://dummyjson.com/users");
+    if (!res.ok) {
+      console.log("Erorr !");
+    }
+    const data = await res.json();
+    console.log(data);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+fetchUser();
